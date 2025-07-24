626c528c28d5b53876c3ca5f53d024ba21effab0
Sarah Clark <sarahireneclark@gmail.com>
2025-07-24 07:14:56 -0500

    Updated to use Swift as a base rather than Objective-C

---
+++ PhotoSafe/PhotoSafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoSafe
+//
+//  Created by Sarah Clark on 7/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
